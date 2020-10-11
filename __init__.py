8f78f872da3ee14f0fdbfce9a6ba090578139e45
BenRobertson150 <benrobertson150@hotmail.co.uk>
2020-10-11 17:56:05 +0100

    Added Skill and test dialog for adding recipe

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_file_handler,intent_handler
 
 
 class ChefAssistant(MycroftSkill):
@@ -9,7 +9,14 @@ class ChefAssistant(MycroftSkill):
     def handle_assistant_chef(self, message):
         self.speak_dialog('assistant.chef')
 
+    @intent_handler('add.new.recipe')
+    def handle_add_new_recipe(self,message):
+        self.speak.dialog('add.recipe')
+        recipe_name = self.get_response('what.is.the.recipe.name')
+        self.speak_dialog('confirm.recipe.name', {'recipe_name': recipe_name})
 
 def create_skill():
     return ChefAssistant()
+
+
 
